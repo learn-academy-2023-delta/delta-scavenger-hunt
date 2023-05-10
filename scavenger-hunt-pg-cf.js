0790bae4381b3bcc6513b64d721c9241226adc0b
Phalco1 <elphalco1@gmail.com>
2023-05-10 09:29:39 -0500

    sickness update

--- scavenger-hunt-pg-cf.js
+++ scavenger-hunt-pg-cf.js
@@ -1,3 +1,4 @@
 // Im sick as a dog. i just took 2 ibprophin*.
 // You should definitely get some sleep tonight, and don't forget to hydrate!
 // Thank you. I'll be sure to rest and drink lots of water.
+// Good morning! I feel even better today, but i feel like its gone be a few more days until im %100.
